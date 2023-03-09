18daf071f21d5f39680fb94f70f8ac04922b5a62
Adebola07 <07zamani@gmail.com>
2023-03-09 18:41:53 -0500

    Its working

--- script.groovy
+++ script.groovy
@@ -1,20 +1,22 @@
 def buildJar() {
     echo "building the application..."
+    sh "mvn clean"
     sh 'mvn package'
 } 
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t adebola07/flaskapp:5.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push adebola07/flaskapp:5.0'
+    withCredentials([usernamePassword(credentialsId: 'nexus=cred', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t 44.212.69.70:8080/flaskapp:5.0 .'
+        sh 'echo $PASS | docker login -u $USER --password-stdin 44.212.69.70:8080'
+        sh 'docker push 44.212.69.70:8080/flaskapp:5.0'
     }
 } 
 
 def deployApp() {
     echo 'deploying the application...'
     echo 'My name is zamani'
+    echo "Yes we made it by God's grace"
 } 
 
 return this
