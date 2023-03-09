3fd2623492fef7a336b63bd6a17db15cd5b36a57
Adebola07 <07zamani@gmail.com>
2023-03-09 18:27:25 -0500

    commit

--- script.groovy
+++ script.groovy
@@ -7,7 +7,7 @@ def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t adebola07/flaskapp:5.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'echo $PASS | docker login -u $USER --password-stdin'
         sh 'docker push adebola07/flaskapp:5.0'
     }
 } 
