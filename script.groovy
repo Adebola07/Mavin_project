7fd18d551b7da00bb423bb455e6d9b1475e3aba6
Adebola07 <07zamani@gmail.com>
2023-03-22 12:29:37 -0400

    pushing to myapp repo

--- script.groovy
+++ script.groovy
@@ -7,9 +7,9 @@ def buildJar() {
 def buildImage() {
     echo "what to do for you laidis..."
     withCredentials([usernamePassword(credentialsId: 'nexus-cred', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh "docker build -t 34.207.191.12:8080/flaskapp:${imageId} ."
+        sh "docker build -t 34.207.191.12:8080/myapp:${imageId} ."
         sh 'echo $PASS | docker login -u $USER --password-stdin 34.207.191.12:8080'
-        sh "docker push 34.207.191.12:8080/flaskapp:${imageId}"
+        sh "docker push 34.207.191.12:8080/myapp:${imageId}"
     }
 } 
 
@@ -28,7 +28,7 @@ def deployApp() {
 
 def incrementingVersion() {
     sh 'mvn build-helper:parse-version versions:set \
-        -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} \
+        -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.nextMinorVersion}.\\\${parsedVersion.nextIncrementalVersion} \
         versions:commit'
     def version = readFile ('pom.xml') =~ '<version>(.+)</version>'
     def image = version[0][1]
